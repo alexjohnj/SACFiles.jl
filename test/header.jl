6b6dee094b9ada0edca65953fce4b866409f86c1
Alex Jackson <alex@alexj.org>
2016-02-18 20:57:46 -0700

    Add enum for undefined header enumerations

--- test/header.jl
+++ test/header.jl
@@ -3,7 +3,7 @@ using Base.Test
 using SACFiles
 
 # Test enums are defined correctly.
-ids = vcat(1:50, 52:97, 103)
+ids = vcat(-12345, 1:50, 52:97, 103)
 enums = instances(SACHeaderEnum) # This feels dirty. Is instances guaranteed to return the Enums in order?
 map(ids, enums) do id, enum
     @test SACHeaderEnum(id) == enum
