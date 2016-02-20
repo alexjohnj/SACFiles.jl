742e514817c293f2d7acc56f958b1fae6dd5a55c
Alex Jackson <alex@alexj.org>
2016-02-19 21:50:49 -0700

    Add synthetic seismo header reading tests

--- test/header.jl
+++ test/header.jl
@@ -33,4 +33,57 @@ open("./test-files/test-hexed-header.sac", "r") do f
     end
 end
 
+# Test header reading on a synthetic seismogram
+open("./test-files/test-seismo.sac", "r") do f
+    inhdr = readsachdr(f)
+    testhdr = SACDataHeader(Int32(1000), Float32(9.459999), Float32(1.945E1), SACFiles.itime, true, Float32(1E-2))
+    testhdr.IDEP = SACFiles.ivolts
+    testhdr.DEPMIN = -1.569280
+    testhdr.DEPMAX = 1.520640
+    testhdr.DEPMEN = -9.854718E-2
+    testhdr.O = -41.43 # Equivalent of OMARKER variable
+    testhdr.A = 10.464 # Equivalent of AMARKER variable
+    testhdr.IZTYPE = SACFiles.ib
+    testhdr.KSTNM = rpad("CDV", 8, " ")
+    testhdr.CMPAZ = 0
+    testhdr.CMPINC = 0
+    testhdr.STLA = 4.8E1
+    testhdr.STLO = -1.2E2
+    testhdr.KEVNM = rpad("K8108838", 16, " ")
+    testhdr.EVLA = 4.8E1
+    testhdr.EVLO = -1.25E2
+    testhdr.EVDP = 1.5E1
+    testhdr.IEVTYP = SACFiles.ipostq
+    testhdr.DIST = 3.730627e+02
+    testhdr.AZ = 8.814721e+01
+    testhdr.BAZ = 2.718528e+02
+    testhdr.GCARC = 3.357465e+00
+    testhdr.LOVROK = true
+    testhdr.NVHDR = 6
+    testhdr.NORID = 0
+    testhdr.NEVID = 0
+    testhdr.LPSPOL = true
+    testhdr.LCALDA = true
+
+    testhdr.NZYEAR = 1981
+    testhdr.NZJDAY = 88
+    testhdr.NZHOUR = 10
+    testhdr.NZMIN = 38
+    testhdr.NZSEC = 14
+    testhdr.NZMSEC = 0
+
+
+    for val in SACFiles.sacheader_variables
+        if val == :UNUSED || val == :INTERNAL
+            continue
+        end
+
+        if typeof(testhdr.(val)) == Float32
+            @test_approx_eq inhdr.(val) testhdr.(val)
+        else
+            @test inhdr.(val) == testhdr.(val)
+        end
+    end
+end
+
 end
