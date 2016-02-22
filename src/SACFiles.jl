5873666b564204fd5959dda8964422ef0c5b3b38
Alex Jackson <alex@alexj.org>
2016-02-21 19:47:03 -0700

    Declare SAC file data reading functions with docs

--- src/SACFiles.jl
+++ src/SACFiles.jl
@@ -3,6 +3,7 @@ module SACFiles
 export SACHeaderEnum, SACDataHeader, readsachdr
 export AbstractSACData, AbstractSACSpectrum, AbstractSACTimeSeries
 export SACEvenTimeSeries, SACUnevenTimeSeries, SACAmplitudeSpectrum, SACComplexSpectrum, SACGeneralXY
+export readsac, readsac_eventime, readsac_uneventime, readsac_rlim, readsac_amph, readsac_xy
 
 include("header.jl")
 include("data_reading.jl")
--- src/data_reading.jl
+++ src/data_reading.jl
@@ -31,3 +31,54 @@ type SACGeneralXY <: AbstractSACData
     x::Vector{Float32}
     y::Vector{Float32}
 end
+
+"Read the SAC file stream `f`. Returns a subtype of `AbstractSACData` determined
+using the file's header's `iftype` and `leven` variables. Note that this
+function isn't type-stable and so shouldn't be used in performance sensitive
+code."
+readsac(fname::AbstractString) = open(readsac, fname)
+function readsac(f::IOStream)
+    hdr = readsachdr(f)
+
+    if hdr.iftype == itime && hdr.leven == true
+        readsac_eventime(f, hdr)
+    elseif hdr.iftype == itime && hdr.leven == false
+        readsac_uneventime(f, hdr)
+    elseif hdr.iftype == irlim
+        readsac_rlim(f, hdr)
+    elseif hdr.iftype == iamph
+        readsac_amph(f, hdr)
+    elseif hdr.iftype == ixy
+        readsac_xy(f, hdr)
+    end
+end
+
+"Read an evenly spaced time series SAC file from the stream `f`. Returns an
+instance of `SACEvenTimeSeries`."
+readsac_eventime(f::IOStream) = readsac_eventime(f, readsachdr(f))
+function readsac_eventime(f::IOStream, hdr::SACDataHeader)
+end
+
+"Read an unevenly spaced time series SAC file from the stream `f`. Returns an
+instance of `SACUnevenTimeSeries`."
+readsac_uneventime(f::IOStream) = readsac_uneventime(f, readsachdr(f))
+function readsac_uneventime(f::IOStream, hdr::SACDataHeader)
+end
+
+"Read an amplitude/phase SAC file from the stream `f`. Returns an instance of
+`SACAmplitudeSpectrum`."
+readsac_amph(f::IOStream) = readsac_amph(f, readsachdr(f))
+function readsac_amph(f::IOStream, hdr::SACDataHeader)
+end
+
+"Read a complex/imaginary SAC file from the stream `f`. Returns an instance of
+`SACComplexSpectrum`."
+readsac_rlim(f::IOStream) = readsac_rlim(f, readsachdr(f))
+function readsac_rlim(f::IOStream, hdr::SACDataHeader)
+end
+
+"Read a general XY sac file from the stream `f`. Returns an instance of
+`SACGenrealXY`."
+readsac_xy(f::IOStream) = readsac_xy(f, readsachdr(f))
+function readsac_xy(f::IOStream, hdr::SACDataHeader)
+end
