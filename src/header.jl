7fe25607318f7c1ab2565b36a05c5602f5e6967b
Alex Jackson <alex@alexj.org>
2016-02-23 21:52:03 -0700

    Add docstrings for header fields

--- src/header.jl
+++ src/header.jl
@@ -64,68 +64,128 @@ See Also
 `SACHeaderEnum` for information on the possible enumerated values.
 """
 type SACDataHeader
+    "Increment between evenly spaced samples."
     delta::Float32
+    "Minimum value of the dependent variable."
     depmin::Float32
+    "Maximum value of the dependent variable."
     depmax::Float32
+    "Scale factor for dependent variable (not used by SAC)."
     scale::Float32
+    "Observed increment if different from `delta`."
     odelta::Float32
+    "Beginning value of the independent variable."
     b::Float32
+    "Ending value of the independent variable."
     e::Float32
+    "Event origin time relative to the reference time in seconds."
     o::Float32
+    "First arrival time relative to reference time in seconds."
     a::Float32
     internal1::Float32
+    "User defined time pick or marker."
     t0::Float32
+    "User defined time pick or marker."
     t1::Float32
+    "User defined time pick or marker."
     t2::Float32
+    "User defined time pick or marker."
     t3::Float32
+    "User defined time pick or marker."
     t4::Float32
+    "User defined time pick or marker."
     t5::Float32
+    "User defined time pick or marker."
     t6::Float32
+    "User defined time pick or marker."
     t7::Float32
+    "User defined time pick or marker."
     t8::Float32
+    "User defined time pick or marker."
     t9::Float32
+    "End of event time in seconds relative to reference time."
     f::Float32
+    "Instrument response parameter (not used by SAC)."
     resp0::Float32
+    "Instrument response parameter (not used by SAC)."
     resp1::Float32
+    "Instrument response parameter (not used by SAC)."
     resp2::Float32
+    "Instrument response parameter (not used by SAC)."
     resp3::Float32
+    "Instrument response parameter (not used by SAC)."
     resp4::Float32
+    "Instrument response parameter (not used by SAC)."
     resp5::Float32
+    "Instrument response parameter (not used by SAC)."
     resp6::Float32
+    "Instrument response parameter (not used by SAC)."
     resp7::Float32
+    "Instrument response parameter (not used by SAC)."
     resp8::Float32
+    "Instrument response parameter (not used by SAC)."
     resp9::Float32
+    "Station latitude with degrees north being positive."
     stla::Float32
+    "Station longitude with degrees east being positive."
     stlo::Float32
+    "Station elevation above sea level in metres."
     stel::Float32
+    "Station depth below the surface in metres."
     stdp::Float32
+    "Event latitude with degrees north being positive."
     evla::Float32
+    "Event longitude with degrees east being positive."
     evlo::Float32
+    "Event elevation in metres."
     evel::Float32
+    "Event depth below surface in kilometres."
     evdp::Float32
+    "Event magnitude."
     mag::Float32
+    "User defined variable storage area."
     user0::Float32
+    "User defined variable storage area."
     user1::Float32
+    "User defined variable storage area."
     user2::Float32
+    "User defined variable storage area."
     user3::Float32
+    "User defined variable storage area."
     user4::Float32
+    "User defined variable storage area."
     user5::Float32
+    "User defined variable storage area."
     user6::Float32
+    "User defined variable storage area."
     user7::Float32
+    "User defined variable storage area."
     user8::Float32
+    "User defined variable storage area."
     user9::Float32
+    "Station to event distance in kilometres."
     dist::Float32
+    "Event to station azimuth in degrees."
     az::Float32
+    "Station to event azimuth in degrees."
     baz::Float32
+    "Station to event great circle length (degrees)>"
     gcarc::Float32
     internal2::Float32
     internal3::Float32
+    "Mean value of the dependent variable."
     depmen::Float32
+    "Component azimuth in degrees measured clockwise from north."
     cmpaz::Float32
+    "Component incident angle measured in degrees from the vertical."
     cmpinc::Float32
+    "Minimum value of X (spectral files only)."
     xminimum::Float32
+    "Maximum value of X (spectral files only)."
     xmaximum::Float32
+    "Minimum value of Y (spectral files only)."
     yminimum::Float32
+    "Maximum value of Y (spectral files only)."
     ymaximum::Float32
     unused1::Float32
     unused2::Float32
@@ -135,33 +195,136 @@ type SACDataHeader
     unused6::Float32
     unused7::Float32
     # Integers
+    "GMT year corresponding to reference (zero) time in file."
     nzyear::Int32
+    "GMT julian day corresponding to reference (zero) time in file."
     nzjday::Int32
+    "GMT hour corresponding to reference (zero) time in file."
     nzhour::Int32
+    "GMT minute corresponding to reference (zero) time in file."
     nzmin::Int32
+    "GMT second corresponding to reference (zero) time in file."
     nzsec::Int32
+    "GMT millisecond corresponding to reference (zero) time in file."
     nzmsec::Int32
+    "Header version number"
     nvhdr::Int32
+    "Origin ID (CSS 3.0)"
     norid::Int32
+    "Event ID (CSS 3.0)"
     nevid::Int32
+    "Number of points per data component"
     npts::Int32
     internal4::Int32
+    "Waveform ID (CSS 3.0)"
     nwfid::Int32
+    "Spectral length (spectral files only)."
     nxsize::Int32
+    "Spectral width (spectral files only)."
     nysize::Int32
     unused8::Int32
     # Enumerations
+    """Type of file. Must be one of:
+
+     - `itime` (Time series file)
+     - `irlim` (Spectral file with real and imaginary components)
+     - `iamph` (Spectral file with amplitude and phase components)
+     - `ixy` (General x vs. y data)
+     - `ixyz` (General xyz data)
+    """
     iftype::SACHeaderEnum
+    """Type of dependent variable. Possible values include:
+
+    - `iunkn` (Unknown)
+    - `idisp` (Displacement in nm)
+    - `ivel` (Velocity in nm/sec)
+    - `ivolts` (Velocity in volts)
+    - `iacc` (Acceleration in nm/sec/sec)
+    """
     idep::SACHeaderEnum
+    """Reference time type. Possible values include:
+
+    - `iunkn` (Unknown)
+    - `ib` (Begin time)
+    - `iday` (Midnight of reference GMT day)
+    - `io` (Event origin time)
+    - `ia` (First arrival time)
+    - `itn` where n = 0:9. (User defined time pick)
+    """
     iztype::SACHeaderEnum
     unused9::SACHeaderEnum
+    "Type of recording instrument (not used by SAC)."
     iinst::SACHeaderEnum
+    "Station geographic region (not used by SAC)."
     istreg::SACHeaderEnum
+    "Event geographic region (not used by SAC)."
     ievreg::SACHeaderEnum
+    """Type of event. One of:
+
+    - `iunkn` (Unknown)
+    - `inucl` (Nuclear event)
+    - `ipren` (Nuclear pre-shot event)
+    - `ipostn` (Nuclear post-shot event)
+    - `iquake` (Earthquake)
+    - `ipreq` (Foreshock)
+    - `ipostq` (Aftershock)
+    - `ichem` (Chemical explosion)
+    - `iqb` (Quarry or mine blast confirmed by quarry)
+    - `iqb1` (Quarry/mine blast with designed shot info-ripple fired)
+    - `iqb2` (Quarry/mine blast with observed shot info-ripple fired)
+    - `iqbx` (Quarry or mine blast - single shot)
+    - `iqmt` (Quarry/mining-induced events: tremors and rockbursts)
+    - `ieq` (Earthquake)
+    - `ieq1` (Earthquakes in a swarm or aftershock sequence)
+    - `ieq2` (Felt earthquake)
+    - `ime` (Marine explosion)
+    - `iex` (Other explosion)
+    - `inu` (Nuclear explosion)
+    - `inc` (Nuclear cavity collapse)
+    - `io` (Other source of unknown origin)
+    - `il` (Local event of unknown origin)
+    - `ir` (Regional event of unknown origin)
+    - `it` (Teleseismic event of unknown origin)
+    - `iu` (Undetermined or conflicting information)
+    - `iother` (Other)
+    """
     ievtyp::SACHeaderEnum
+    """Quality of data (not used by SAC). Can be one of:
+
+    - `igood` (Good data)
+    - `iglch` (Glitches)
+    - `idrop` (Dropouts)
+    - `ilowsn` (Low signal to noise ratio)
+    - `iother` (Other)
+    """
     iqual::SACHeaderEnum
+    "Synthetic data flag (not used by SAC). Can be `irldta` (real data)"
     isynth::SACHeaderEnum
+    """Magnitude type. One of:
+
+    - `imb` (Body wave magnitude)
+    - `ims` (Surface wave magnitude)
+    - `iml` (Local magnitude)
+    - `imw` (Moment magnitude)
+    - `imd` (Duration magnitude)
+    - `imx` (User defined magnitude)
+    """
     imagtyp::SACHeaderEnum
+    """Source of magnitude information. One of:
+
+    - `ineic` (National Earthquake Information Center)
+    - `ipde` (Preliminary Determination of Epicentre)
+    - `iisc` (International Seismological Centre)
+    - `ireb` (Review Event Bulletin)
+    - `iusgs` (United States Geological Survey)
+    - `ibrk` (UC Berkeley)
+    - `icaltech` (California Institute of Technology)
+    - `illnl` (Lawrence Livermore National Laboratory)
+    - `ievloc` (Event Location (computer program))
+    - `ijsop` (Join Seismic Observation Program)
+    - `iuser` (The individual using SAC2000)
+    - `iunknown` (Unknown)
+    """
     imagsrc::SACHeaderEnum
     unused10::SACHeaderEnum
     unused11::SACHeaderEnum
@@ -172,34 +335,62 @@ type SACDataHeader
     unused16::SACHeaderEnum
     unused17::SACHeaderEnum
     # Logical Variables
+    "`true` if data is evenly spaced."
     leven::Bool
+    "`true` if station components have a positive polarity (left-hand rule)."
     lpspol::Bool
+    "`true` if it is OK to overwrite this file on disk."
     lovrok::Bool
+    "True if `dist`, `az`, `baz` and `gcarc` are to be calculated from station
+    and event coordinates."
     lcalda::Bool
     unused18::Bool
     # Alphanumerics
+    "Station name."
     kstnm::ASCIIString
+    "Event name."
     kevnm::ASCIIString
+    "If nuclear related then the hole identifier. Otherwise, the location identifier."
     khole::ASCIIString
+    "Event origin time identification."
     ko::ASCIIString
+    "First arrival time identification."
     ka::ASCIIString
+    "User defined time pick identifier."
     kt0::ASCIIString
+    "User defined time pick identifier."
     kt1::ASCIIString
+    "User defined time pick identifier."
     kt2::ASCIIString
+    "User defined time pick identifier."
     kt3::ASCIIString
+    "User defined time pick identifier."
     kt4::ASCIIString
+    "User defined time pick identifier."
     kt5::ASCIIString
+    "User defined time pick identifier."
     kt6::ASCIIString
+    "User defined time pick identifier."
     kt7::ASCIIString
+    "User defined time pick identifier."
     kt8::ASCIIString
+    "User defined time pick identifier."
     kt9::ASCIIString
+    "End of event time identification."
     kf::ASCIIString
+    "User defined variable storage area."
     kuser0::ASCIIString
+    "User defined variable storage area."
     kuser1::ASCIIString
+    "User defined variable storage area."
     kuser2::ASCIIString
+    "Channel name."
     kcmpnm::ASCIIString
+    "Name of the seismic network."
     knetwk::ASCIIString
+    "Date data was read onto computer."
     kdatrd::ASCIIString
+    "Generic name of recording instrument."
     kinst::ASCIIString
 
     function SACDataHeader(npts::Int32, beginning::Float32, ending::Float32,
