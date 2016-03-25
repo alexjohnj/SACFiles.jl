fe9d1aff748f06e6153ac0271dcbbb971d285658
Alex Jackson <alex@alexj.org>
2016-03-24 21:07:33 -0600

    Rewrite header tests to use test sets

--- test/header.jl
+++ test/header.jl
@@ -1,50 +1,48 @@
-module TestHeaders
-using Base.Test
-using SACFiles
-import SACTestUtilities
-
-# Test enums are defined correctly.
-ids = vcat(-12345, 1:50, 52:97, 103)
-enums = instances(HeaderEnum) # This feels dirty. Is instances guaranteed to return the Enums in order?
-map(ids, enums) do id, enum
-    @test HeaderEnum(id) == enum
-end
-
-# Make sure we're reading the whole header
-open("./test-files/test-seismo.sac", "r") do f
-    _ = readsachdr(f)
-    @test position(f) == 632
-end
-
-# Test header reading on a hexed file.
-open("./test-files/test-hexed-header.sac", "r") do f
-    inhdr = readsachdr(f)
-    hdr_hexedvalues = Dict{Type, Any}(Float32       => Float32(1337.0),
-                                      Int32         => Int32(1337),
-                                      HeaderEnum => SACFiles.inucl,
-                                      ASCIIString   => ascii("BLEEPBLO"),
-                                      Bool          => true)
-    testhdr = Header()
-
-    map(fieldnames(Header), Header.types) do field, T
-        if field == :kevnm
-            testhdr.(field) = ascii("BLEEPBLOOPBLEEPS")
-        elseif field == :nvhdr
-            testhdr.(field) = 6
-        else
-            testhdr.(field) = hdr_hexedvalues[T]
+@testset "Header Tests" begin
+    @testset "Header Enums" begin
+        let
+            ids = vcat(-12345, 1:50, 52:97, 103)
+            enums = collect(instances(HeaderEnum))
+            @test enums == map(HeaderEnum, ids)
         end
-        @test inhdr.(field) == testhdr.(field)
     end
-end
 
-# Test header reading functions on a synthetic SAC generated seismogram
-testhdr = SACTestUtilities.make_test_seismo_hdr()
-open("./test-files/test-seismo.sac", "r") do f
-    inhdr = readsachdr(f)
-    SACTestUtilities.testhdrequal(inhdr, testhdr)
-end
-SACTestUtilities.testhdrequal(readsachdr("./test-files/test-seismo.sac"), testhdr)
-SACTestUtilities.testhdrequal(readsachdr("./test-files/be-test-seismo.sac"), testhdr)
+    @testset "Header Reading" begin
+        # Make sure we're reading the whole header
+        open("./test-files/test-seismo.sac", "r") do f
+            _ = readsachdr(f)
+            @test position(f) == 632
+        end
 
+        # Test header reading on a hexed file.
+        open("./test-files/test-hexed-header.sac", "r") do f
+            inhdr = readsachdr(f)
+            hdr_hexedvalues = Dict{Type, Any}(Float32     => Float32(1337.0),
+                                              Int32       => Int32(1337),
+                                              HeaderEnum  => SACFiles.inucl,
+                                              ASCIIString => ascii("BLEEPBLO"),
+                                              Bool        => true)
+            testhdr = Header()
+
+            map(fieldnames(Header), Header.types) do field, T
+                if field == :kevnm
+                    testhdr.(field) = ascii("BLEEPBLOOPBLEEPS")
+                elseif field == :nvhdr
+                    testhdr.(field) = 6
+                else
+                    testhdr.(field) = hdr_hexedvalues[T]
+                end
+                @test inhdr.(field) == testhdr.(field)
+            end
+        end
+
+        # Test header reading functions on a synthetic SAC generated seismogram
+        testhdr = SACTestUtilities.make_test_seismo_hdr()
+        open("./test-files/test-seismo.sac", "r") do f
+            inhdr = readsachdr(f)
+            SACTestUtilities.testhdrequal(inhdr, testhdr)
+        end
+        SACTestUtilities.testhdrequal(readsachdr("./test-files/test-seismo.sac"), testhdr)
+        SACTestUtilities.testhdrequal(readsachdr("./test-files/be-test-seismo.sac"), testhdr)
+    end
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,8 +5,9 @@ else
     using BaseTestNext
     const Test = BaseTestNext
 end
-
 include("test_utilities.jl")
+import SACTestUtilities
+
 include("header.jl")
 include("data.jl")
 include("io_utils.jl")
